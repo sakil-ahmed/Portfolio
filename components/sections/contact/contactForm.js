2db5df51cb50cacf0e884ff66936e45cce3a30dd
sakil-ahmed <sakilahmed1495@gmail.com>
2022-12-13 20:55:25 +0600

    update

--- components/sections/contact/contactForm.js
+++ components/sections/contact/contactForm.js
@@ -1,26 +1,15 @@
 import React, { useRef, useEffect } from "react";
 import StyleForm from "./styleForm";
 import { useForm } from "@formspree/react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("myyvrqkk");
 
-  const form = useRef();
+  const Contactform = useRef();
 
   useEffect(() => {
     if (state.succeeded) {
-      form.current.reset();
-      toast.success("Submit Successfull", {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-      });
+      Contactform.current.reset();
     } else {
       // Nothing to do
     }
@@ -28,7 +17,7 @@ const ContactForm = () => {
 
   return (
     <>
-      <StyleForm method="POST" ref={form} onSubmit={handleSubmit}>
+      <StyleForm method="POST" ref={Contactform} onSubmit={handleSubmit}>
         <div className="display_flex">
           <div className="input_group">
             <input
@@ -92,7 +81,6 @@ const ContactForm = () => {
           </button>
         </div>
       </StyleForm>
-      <ToastContainer />
     </>
   );
 };
