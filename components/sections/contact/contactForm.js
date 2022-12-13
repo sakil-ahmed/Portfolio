10d0e7af81ba13a9c72f60f52e99025bf5d2d806
sakil-ahmed <sakilahmed1495@gmail.com>
2022-12-13 20:44:47 +0600

    update Contact form

--- components/sections/contact/contactForm.js
+++ components/sections/contact/contactForm.js
@@ -1,86 +1,99 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import StyleForm from "./styleForm";
+import { useForm } from "@formspree/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
-  const [message, setMessage] = useState({});
+  const [state, handleSubmit] = useForm("myyvrqkk");
 
   const form = useRef();
 
-  const handleChange = (e) => {
-    setMessage({ ...message, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(message);
-    form.current.reset();
-  };
+  useEffect(() => {
+    if (state.succeeded) {
+      form.current.reset();
+      toast.success("Submit Successfull", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      // Nothing to do
+    }
+  }, [state]);
 
   return (
-    <StyleForm ref={form} onSubmit={handleSubmit}>
-      <div className="display_flex">
-        <div className="input_group">
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="name"
-            placeholder="Your Name"
-            onChange={handleChange}
-            required
-          />
+    <>
+      <StyleForm method="POST" ref={form} onSubmit={handleSubmit}>
+        <div className="display_flex">
+          <div className="input_group">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="name"
+              placeholder="Your Name"
+              required
+              spellCheck="false"
+            />
+          </div>
+          <div className="input_group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="email"
+              placeholder="Your Email"
+              required
+              spellCheck="false"
+            />
+          </div>
+          <div className="input_group">
+            <input
+              type="number"
+              name="number"
+              className="phone"
+              id="phone"
+              placeholder="Your Phone"
+              required
+              spellCheck="false"
+            />
+          </div>
+          <div className="input_group">
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              className="subject"
+              placeholder="Subject"
+              required
+              spellCheck="false"
+            />
+          </div>
+          <div className="input_group">
+            <textarea
+              id="message"
+              name="message"
+              className="message"
+              placeholder="Write your message here"
+              required
+              spellCheck="false"
+            ></textarea>
+          </div>
         </div>
-        <div className="input_group">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="email"
-            placeholder="Your Email"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input_group">
-          <input
-            type="number"
-            name="number"
-            className="phone"
-            id="phone"
-            placeholder="Your Phone"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input_group">
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            className="subject"
-            placeholder="Subject"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input_group">
-          <textarea
-            id="message"
-            name="message"
-            className="message"
-            placeholder="Write your message here"
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-      </div>
 
-      <div className="btn_group">
-        <button className="btn">
-          <span>Submit Now</span>
-        </button>
-      </div>
-    </StyleForm>
+        <div className="btn_group">
+          <button type="submit" className="btn">
+            <span>Submit Now</span>
+          </button>
+        </div>
+      </StyleForm>
+      <ToastContainer />
+    </>
   );
 };
 
